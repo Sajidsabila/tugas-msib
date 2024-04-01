4a66767b3e2d557ab06b0b4203097c4f627530d4
Sajidsabila <129641654+Sajidsabila@users.noreply.github.com>
2024-04-01 12:47:17 +0700

    Add files via upload

---
+++ 3 Mins Beads/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $.ajax({
+    url: "product.json",
+    dataType: "json",
+    success: function (data) {
+      console.log(data);
+      $.each(data, function (index, products) {
+        $("#product-card").append(
+          `
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="card m-4" >
+                    <div class="card-header bg-primary text-white fw-bold">
+                        ${products.name}
+                    </div>
+                    <div class="card-body">
+                     <img src="${products.image}" class="image-fluid rounded-3" width="100%" alt="${products.name}">
+                     <p class="">${products.description}</p>
+                     <p class="">${products.price}</p>
+                     <p class=""><button class="btn btn-primary">Beli</button>
+                     <button class="btn btn-success">Detail</button>           
+                     </p>
+                </div>
+                </div>`
+        );
+      });
+    },
+  });
+});
